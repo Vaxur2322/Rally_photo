a59ec53fb3d22ced25c2c9f9df6174280a6c0a14
Vaxur2322 <yanaelcapitaine3101@gmail.com>
2026-03-09 00:17:20 +0100

    Use environment variable for server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const UPLOAD_DIR = path.join(__dirname, 'upload');
 
 // Vérification admin côté serveur
